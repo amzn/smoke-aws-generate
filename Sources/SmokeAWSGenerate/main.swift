4f68a87c482bf4921bf2fb51c86fa61b313abde0
Simon Pilkington <simonpi@amazon.com>
2019-04-04 18:00:28 -0700

    Update SmokeHTTP to 1.x.

--- Sources/SmokeAWSGenerate/main.swift
+++ Sources/SmokeAWSGenerate/main.swift
@@ -33,7 +33,7 @@ struct CommonConfiguration {
 }
 
 var isUsage = CommandLine.arguments.count == 2 && CommandLine.arguments[1] == "--help"
-let goRepositoryTag = "v1.19.7"
+let goRepositoryTag = "v1.19.9"
 
 let fileHeader = """
     // Copyright 2018-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
@@ -160,7 +160,7 @@ func generatePackageFile(baseNames: [String]) -> String {
                 .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "1.0.0"),
                 .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", .upToNextMajor(from: "1.0.0")),
                 .package(url: "https://github.com/LiveUI/XMLCoding.git", .upToNextMajor(from: "0.4.0")),
-                .package(url: "https://github.com/amzn/smoke-http.git", .upToNextMajor(from: "0.8.0")),
+                .package(url: "https://github.com/amzn/smoke-http.git", .upToNextMajor(from: "1.0.0")),
             ],
             targets: [\n
         """
