8dc5dd351a03bf3bd23e6df63a04485427d3148d
Simon Pilkington <simonmpilkington@icloud.com>
2020-05-05 14:19:10 -0700

    Update dependency to 2.0.0.

--- Package.swift
+++ Package.swift
@@ -35,7 +35,7 @@ let package = Package(
             targets: ["CoralToJSONServiceModel"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/amzn/service-model-swift-code-generate.git", from: "2.0.0-beta.1"),
+        .package(url: "https://github.com/amzn/service-model-swift-code-generate.git", from: "2.0.0"),
     ],
     targets: [
         .target(
