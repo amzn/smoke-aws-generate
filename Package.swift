2779c80e2c2830dfb0e2fd8eaba8462873f77b49
Simon Pilkington <simonpi@amazon.com>
2022-10-05 10:00:21 +1100

    Update swift-argument-parser dependency.

--- Package.swift
+++ Package.swift
@@ -48,7 +48,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/amzn/service-model-swift-code-generate.git", from: "3.0.0-beta.14"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
         .package(url: "https://github.com/amzn/openapi-swift-code-generate.git", from: "1.0.0-beta.1"),
     ],
     targets: [
