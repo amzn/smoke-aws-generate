388cf1e145ab76385f6c0c09afee3bf296e4b586
Simon Pilkington <simonpi@amazon.com>
2022-10-14 08:19:37 +1100

    Update service-model-swift-code-generate dependency.

--- Sources/APIGatewayClientModelGenerate/APIGatewayClientCodeGeneration+generateClientApplicationFiles.swift
+++ Sources/APIGatewayClientModelGenerate/APIGatewayClientCodeGeneration+generateClientApplicationFiles.swift
@@ -104,7 +104,7 @@ extension APIGatewayClientCodeGenerator where TargetSupportType: ModelTargetSupp
                     .package(url: "https://github.com/amzn/smoke-aws-support.git", from: "1.0.0"),
                     .package(url: "https://github.com/amzn/smoke-http.git", from: "2.14.0"),
                     .package(url: "https://github.com/apple/swift-log", from: "1.0.0"),
-                    .package(url: "https://github.com/amzn/smoke-aws-generate.git", from: "3.0.0-beta.5"),
+                    .package(url: "https://github.com/amzn/smoke-aws-generate.git", from: "3.0.0-rc.1"),
                     ],
                 targets: [
                     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
