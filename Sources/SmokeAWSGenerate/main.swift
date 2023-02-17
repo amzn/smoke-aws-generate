388c1e0856af0c94a6cb0c834bc7caf8e59cf756
Simon Pilkington <simonpi@amazon.com>
2023-02-17 16:32:28 +1100

    Update repository version.

--- Sources/SmokeAWSGenerate/main.swift
+++ Sources/SmokeAWSGenerate/main.swift
@@ -36,7 +36,7 @@ struct CommonConfiguration {
 }
 
 var isUsage = CommandLine.arguments.count == 2 && CommandLine.arguments[1] == "--help"
-let goRepositoryTag = "v1.44.114"
+let goRepositoryTag = "v1.44.174"
 
 let fileHeader = """
     // Copyright 2018-2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
