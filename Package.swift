a0d480eddf575957f2148766b1f5e0749e2a55cd
Konstantin Novichikhin <konstantin.novichikhin@gmail.com>
2024-05-27 23:57:40 -0400

    Update SwaggerParser repository location because original is 404

--- Package.swift
+++ Package.swift
@@ -35,7 +35,7 @@ let package = Package(
             targets: ["CoralToJSONServiceModel"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/amzn/service-model-swift-code-generate.git", from: "2.6.0"),
+        .package(url: "https://github.com/amzn/service-model-swift-code-generate.git", from: "2.6.1"),
     ],
     targets: [
         .target(
